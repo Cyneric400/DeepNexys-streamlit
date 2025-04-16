78ae9529a5ccbabf27168053a71b1def836eb55b
Rhys Sharpe <davidrsharpe@southern.edu>
2025-04-16 09:19:32 -0400

    functioning (but clunky) local LLM integration

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
 import streamlit as st
+import llm_man
+
+if 'LLM' not in st.session_state:
+    st.session_state.LLM = llm_man.OLLAMA(model_name="gemma3")
+if 'prompt' not in st.session_state:
+    st.session_state.prompt = ''
+def send_prompt():
+    response = st.session_state.LLM.predict(st.session_state.prompt)
+    print(response)
+    st.write(response)
 
 st.title("DeepNexys")
 st.subheader("Your Hub for New Ideas")
 
+
 st.file_uploader(label="Upload a PDF", type="pdf", accept_multiple_files=True)
 
-st.text_input(label="Chat with Nexy about your notes!")
+with st.form("nexychat"):
+    st.subheader("Chat with Nexy about your notes!")
+    cols = st.columns([0.92,0.08], vertical_alignment="bottom")
+    with cols[0]:
+        prompt = st.text_input(label="Enter a prompt...", key="prompt")
+    with cols[1]:
+        submit = st.form_submit_button("^", on_click=send_prompt)
---
+++ llm_man.py
@@ -0,0 +1,36 @@
+# https://medium.com/@rawanalkurd/running-llms-locally-a-guide-to-setting-up-ollama-with-docker-6ef8488e75d4
+
+import requests
+import json
+
+class OLLAMA:
+    def __init__(self, model_name, api_endpoint="http://localhost:11434/api/generate", **kwargs):
+        self.model_name = model_name
+        self.api_endpoint = api_endpoint
+        self.session = requests.Session()
+        self.kwargs = {**kwargs}
+
+        print(f"Initialized {model_name}")
+
+    def predict(self, question, **kwargs):
+        output = ""
+        payload = {'model': self.model_name, 'prompt': question, **self.kwargs, **kwargs}
+        
+        with self.session.post(self.api_endpoint, json=payload, stream=True) as r:
+            print(payload['prompt'])
+            if r.status_code == 200:
+                for line in r.iter_lines():
+                    print(line)
+                    if line:
+                        j = json.loads(line.decode('utf-8'))
+                        output += j.get("response", "")
+                        if j.get("done", True):
+                            break
+            else:
+                print(f"Received error code {r.status_code}")
+        print('success')
+        return output.strip()
+
+    def __call__(self, question, **kwargs):
+        return self.predict(question, **kwargs)
+
