1353dc599d6efa460451f8e344c401281fe77256
Rhys Sharpe <davidrsharpe@southern.edu>
2025-04-17 14:52:51 -0400

    delete old interaction system

--- app.py
+++ app.py
@@ -26,15 +26,6 @@ st.subheader("Your Hub for New Ideas")
 
 st.file_uploader(label="Upload a PDF", type="pdf", accept_multiple_files=True)
 
-# with st.form("nexychat"):
-#     st.subheader("Chat with Nexy about your notes!")
-#     cols = st.columns([0.92,0.08], vertical_alignment="bottom")
-#     with cols[0]:
-#         prompt = st.text_input(label="Enter a prompt...", key="prompt")
-#     with cols[1]:
-#         submit = st.form_submit_button("^", on_click=send_prompt)
-
-# with st.chat_message("user"):
 st.subheader("Chat with Nexy about your notes - now in a new, improved format!")
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
