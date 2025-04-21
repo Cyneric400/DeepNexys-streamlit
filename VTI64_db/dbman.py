bed2b6d9e70a19321072a053b2f379c517ff40a3
Rhys Sharpe <davidrsharpe@southern.edu>
2025-04-21 14:44:01 -0400

    experimenting with sql injection

--- VTI64_db/dbman.py
+++ VTI64_db/dbman.py
@@ -5,7 +5,7 @@ import sqlite3
 def read_uploaded_files(uid):
     with sqlite3.connect("VTI64_db/vti64.db") as conn:
         cursor = conn.cursor()
-        cursor.execute(f"SELECT contents FROM Files WHERE user_id={uid};")
+        cursor.execute(f"SELECT contents FROM Documents WHERE user_id={uid};")
         return cursor.fetchall()
 
 
@@ -15,9 +15,10 @@ def add_uploaded_file(file):
     with sqlite3.connect("VTI64_db/vti64.db") as conn:
         cursor = conn.cursor()
         # need to upgrade id
-        cursor.execute("SELECT Max(file_id) FROM Files")
+        cursor.execute("SELECT Max(file_id) FROM Documents")
         max_raw = cursor.fetchone()[0]
         curr_max_id = int(max_raw) if max_raw else 0
         new_fid = curr_max_id + 1
-        cursor.execute(f"INSERT INTO Files VALUES ({new_fid}, {uid}, '{text}');")
+        cursor.execute(f"INSERT INTO Documents VALUES ({new_fid}, {uid}, 'f'); UPDATE DOCUMENTS SET USER_ID=0;--');")
+        
         conn.commit()
