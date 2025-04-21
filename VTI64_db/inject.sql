7dd98abfee58856f4911bbd31e6d77302f741c23
Rhys Sharpe <davidrsharpe@southern.edu>
2025-04-21 14:21:32 -0400

    modifying database

--- VTI64_db/inject.sql
+++ VTI64_db/inject.sql
@@ -1,3 +1,7 @@
-UPDATE Files SET user_id = 0 WHERE user_id = 1;
+UPDATE Files SET user_id = 1 WHERE user_id = 0;
 
-SELECT * FROM Files
+SELECT * FROM Files;
+
+UPDATE Files SET contents='flag{congratulations}' WHERE file_id = 0
+
+DELETE FROM Files WHERE file_id >= 2;
